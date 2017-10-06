66032dfceab937e0ecb744492d8020ca4e44fa09
egwuenu gift <laurandidi21@gmail.com>
2017-10-06 21:31:06 +0100

    added view page for calling API

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 var express = require("express");
 
+var app = express()
+
+var request = require("request");
+// tells express that there's apublic folder for all assets
+app.use(express.static("public"));
+
+// tells express that the default template enginge is ejs
+app.set("view engine", "ejs");
+
+app.get('/results', function(req, res){
+    request("http://omdbapi.com/?s=games+of+thrones&apikey=thewdb", function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            var data = JSON.parse(body)
+            res.render("results", {data: data});
+        }
+    });
+});
+
+
+app.listen(3000, function () {
+  console.log('Server started at port 3000')
+})
+
