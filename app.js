970f79e8ebdae92e2eb9284dc1a6d6c3f5c5419e
egwuenu gift <laurandidi21@gmail.com>
2017-10-06 21:55:21 +0100

    adding search function to lookup movies

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var express = require("express");
 var app = express()
 
 var request = require("request");
-// tells express that there's apublic folder for all assets
-app.use(express.static("public"));
 
 // tells express that the default template enginge is ejs
 app.set("view engine", "ejs");
 
+app.get('/', function(req, res){
+    res.render('search');
+})
+
 app.get('/results', function(req, res){
-    request("http://omdbapi.com/?s=games+of+thrones&apikey=thewdb", function(error, response, body) {
+    request("http://omdbapi.com/?s=iowa&apikey=thewdb", function(error, response, body) {
         if(!error && response.statusCode == 200) {
             var data = JSON.parse(body)
             res.render("results", {data: data});
